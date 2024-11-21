eaba238690e28dea519053efbb595304c0aabd55
fchsLME <lestes0@ftstudent.org>
2024-11-21 14:02:14 -0500

    ssssss

--- skeleton.gd
+++ skeleton.gd
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-=======
 extends CharacterBody2D
->>>>>>> f445eaa466bbd21b4cf5114aadb5ad2c91429fec
 
 var speed = 80
 var player_chase = false
 var player = null
 
-<<<<<<< HEAD
-=======
 func _physics_process(delta):
 	if player_chase:
 		position += (player.position - position)/speed
@@ -26,4 +21,3 @@ func _on_detection_area_body_entered(body):
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
->>>>>>> f445eaa466bbd21b4cf5114aadb5ad2c91429fec
